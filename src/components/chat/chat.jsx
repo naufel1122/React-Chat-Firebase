990e5b65299b6a1063dbcc4141d3dd650f2e4f56
naufel1122 <nofil.khan037@gmail.com>
2024-08-05 13:22:20 -0700

    working on chats

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc, arrayUnion } from "firebase/firestore";
 import { db } from "../../lib/firebase";
 import { useChatStore } from "../../lib/chatStore";
+import { useUserStore } from "../../lib/userStore";
 
 const Chat = () => {
   const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
-  const { chatId } = useChatStore();
+  const { currentUser } = useUserStore();
+  const { chatId, user } = useChatStore();
   const centerRef = useRef(null);
-
   const endRef = useRef(null);
 
   useEffect(() => {
@@ -28,11 +29,48 @@ const Chat = () => {
     };
   }, [chatId]);
 
-  
-
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
+  };
+
+  const handleSend = async () => {
+    if (text === "") return;
+    try {
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createdAt: new Date(),
+        }),
+      });
+
+      const userIDs = [currentUser.id, user.id];
+
+      userIDs.forEach(async (id) => {
+        const userChatRef = doc(db, "userChats", id);
+        const userChatsSnapshot = await getDoc(userChatRef);
+
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data();
+
+          const chatIndex = userChatsData.chats.findIndex(
+            (c) => c.chatId === chatId
+          );
+
+          userChatsData.chats[chatIndex].lastMessage = text;
+          userChatsData.chats[chatIndex].isSeen =
+            id === currentUser.id ? true : false;
+          userChatsData.chats[chatIndex].updatedAt = Date.now();
+
+          await updateDoc(userChatRef, {
+            chats: userChatsData.chats,
+          });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleScroll = () => {
@@ -71,102 +109,14 @@ const Chat = () => {
         </div>
       </div>
       <div className="center" ref={centerRef} onScroll={handleScroll}>
-        <div className="message">
-          <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
+        {chat?.messages?.map((message) => (
+          <div className="message own" key={message?.createdAt}>
+            <div className="texts">
+              {message.img && <img src={message.img} alt="" />}
+              <p>{message.text}</p>
+            </div>
           </div>
-        </div>
-        <div className="message own">
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message">
-          <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message own">
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message">
-          <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message own">
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message">
-          <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
-        <div className="message own">
-          <div className="texts">
-            <img
-              src="https://images.pexels.com/photos/24405903/pexels-photo-24405903/free-photo-of-beach-umbrellas-and-sunbeds-in-resort-on-sea-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-            />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo
-              veniam odio beatae corporis autem. Corrupti accusamus placeat eum
-              error in animi laudantium odit ad iusto! Voluptatibus magni
-              delectus obcaecati porro.
-            </p>
-            <span>1 min ago</span>
-          </div>
-        </div>
+        ))}
         <div ref={endRef}></div>
       </div>
       <div className="bottom">
@@ -191,7 +141,9 @@ const Chat = () => {
             <EmojiPicker open={open} onEmojiClick={handleEmoji} />
           </div>
         </div>
-        <button className="sendButton">Send</button>
+        <button className="sendButton" onClick={handleSend}>
+          Send
+        </button>
       </div>
     </div>
   );
